1978db02ddd82fcbe2189da35a4f2ab9320b5f39
Sai Prasad <saiprasadronankiloves@gmail.com>
2022-09-29 06:35:35 +0530

    And_gate

---
+++ And_gate/design.sv
@@ -0,0 +1,8 @@
+// Code your design here
+module and_gate(a,b,c);
+  
+  input a,b;
+  output c;
+  assign c=a&b;  
+  
+endmodule
---
+++ And_gate/testbench.sv
@@ -0,0 +1,37 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+   reg a,b;
+  wire c;
+  
+  and_gate dingdong(a,b,c);
+  
+  initial 
+    begin
+      $monitor("a=%0d,b=%0d,c=%0d",a,b,c);
+      
+      a=0;b=0;
+      
+#5
+      a=0;b=1;
+      
+      #5
+      
+     a=1;b=0;
+      
+      #5
+      
+      a=1;b=1;
+      
+      #50 $finish;
+      
+    end 
+  initial
+  begin 
+    
+    $dumpfile("chiku.vcd");
+    $dumpvars;
+    
+  end 
+  
+endmodule
