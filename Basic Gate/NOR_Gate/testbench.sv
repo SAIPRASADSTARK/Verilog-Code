117111e78a5fcb66ddcc1b7cfc8b1338bae32080
Sai Prasad <saiprasadronankiloves@gmail.com>
2022-10-10 06:16:54 +0530

    Nor_Gate

---
+++ Basic Gate/NOR_Gate/testbench.sv
@@ -0,0 +1,42 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg a,b;
+  wire c;
+  
+  nor_gate variables(a,b,c);
+  
+  initial
+    begin 
+      
+    $monitor("a=%0d,b=%0d,c=%0d",a,b,c);
+      
+     a=0;b=0;
+      
+      #5
+      
+
+       a=0;b=1;
+      
+      #5
+      
+       a=1;b=0;
+      
+      #5
+       a=1;b=1;
+      
+      #50 $finish;
+      
+      
+      
+    end
+ 
+  initial
+    begin
+      $dumpfile("wave.vcd");
+      $dumpvars;
+      
+    end
+endmodule
+                
+      
